09fca4d6814d551e11eac5507c6af438784b05a2
leon <xiaqiulei@126.com>
2026-03-08 14:52:29 +0800

    feat: 增加选中代码时的浮动工具栏复制功能
    - 新增 CopyFileInfoAction 动作实现复制逻辑与系统通知
    - 在 plugin.xml 注册动作到 Floating.CodeToolbar 组
    - 增加对应的 i18n 文本

---
+++ src/main/kotlin/dev/leonxia/aifileref/CopyFileInfoAction.kt
@@ -0,0 +1,41 @@
+package dev.leonxia.aifileref
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import java.awt.datatransfer.StringSelection
+
+/**
+ * 编辑器浮动工具栏上的"复制文件信息"操作
+ */
+class CopyFileInfoAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+
+        val info = FileInfoCollector.collectFileInfo(project, virtualFile)
+        val formatted = FileInfoCollector.formatFileInfo(info)
+
+        CopyPasteManager.getInstance().setContents(StringSelection(formatted))
+
+        // 显示通知
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("FileRef.Notification")
+            .createNotification("文件信息已复制到剪切板", NotificationType.INFORMATION)
+            .notify(project)
+    }
+
+    override fun update(e: AnActionEvent) {
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
+        e.presentation.isEnabledAndVisible = virtualFile != null && e.project != null
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+}
